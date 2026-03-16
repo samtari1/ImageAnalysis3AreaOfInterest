2a9846c50a8f16351f02131115e0cf96f0317df4
samtari1 <samtariyang@gmail.com>
2026-03-16 10:51:24 -0400

    Initial Commit

---
+++ ImageAnalysis3AreaOfInterest/ImageAnalysis3AreaOfInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnalysis3AreaOfInterestApp.swift
+//  ImageAnalysis3AreaOfInterest
+//
+//  Created by Quanpeng Yang on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnalysis3AreaOfInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageAnalysis3AreaOfInterestTests/ImageAnalysis3AreaOfInterestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnalysis3AreaOfInterestTests.swift
+//  ImageAnalysis3AreaOfInterestTests
+//
+//  Created by Quanpeng Yang on 3/16/26.
+//
+
+import Testing
+@testable import ImageAnalysis3AreaOfInterest
+
+struct ImageAnalysis3AreaOfInterestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
